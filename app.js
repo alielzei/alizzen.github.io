3cb1ae789b7b0c630e62ea6b16d5f8cc8371fa9a
alizzen <alialibacha@hotmail.com>
2017-05-01 19:41:38 +0300

    :rocket:

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 function sendEmail(){
-	var html = "<h3>Email</h3>"+$('#email').val()
-						+"<h3>Full Name</h3>"+$('#fullName').val()
-						+"<h3>Message</h3>"+$('#message').val()
 
-	$.ajax('https://api.mailgun.net/v3/sandboxcfcb1e9845b74255905573f6ccae0a64.mailgun.org/messages',
+	$.ajax('https://alizzens-services.herokuapp.com/sendEmail',
 	{
 		type:"POST",
-		username: 'api',
-		password: 'key-3edaaabb8c757541b8d89271791949ee',
+		contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
 		data:{
-			"from": $('#email').val()+" <postmaster@sandboxcfcb1e9845b74255905573f6ccae0a64.mailgun.org>",
-			"to": "Ali <alielzein753@gmail.com>",
-			"subject": 'Portfolio Feedback',
-			"html": html,
+			message: $('#message').val(),
+			email: $('#email').val(),
+			fullname: $('#fullname').val()
 		},
 
 		success: function(a,b,c){
-			console.log( 'mail sent: ', b );
 			$('form').css('display', 'none')
 			$('.success').css('display', 'block');
-		}.bind(this),
-
-		
+		},
 
 		error: function( xhr, status, errText ){
 			$('form').css('display', 'none')
@@ -32,9 +24,9 @@ function sendEmail(){
 }
 
 function main(){
-	$('form').submit(function () {
+	$('form').submit(function (e) {
+		e.preventDefault();
 		sendEmail();
-		return false;
 	});
 }
 
