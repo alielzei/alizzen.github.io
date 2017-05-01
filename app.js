aa83cabe3e5bf1c2848c3c8b1675a627118b993a
alizzen <alialibacha@hotmail.com>
2017-05-01 21:52:20 +0300

    :rocket:

--- app.js
+++ app.js
@@ -1,26 +1,62 @@
+function resetErrors(){
+	$('form div p').addClass('hidden');
+	$('form div input').css('border', '1px solid black');
+}
+
 function sendEmail(){
 
-	$.ajax('https://alizzens-services.herokuapp.com/sendEmail',
-	{
-		type:"POST",
-		contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-		data:{
-			message: $('#message').val(),
-			email: $('#email').val(),
-			fullname: $('#fullname').val()
-		},
+	var email = $('#email').val();
+	var pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
+	var message = $('#message').val();
+	var fullname = $('#fullname').val();
 
-		success: function(a,b,c){
-			$('form').css('display', 'none')
-			$('.success').css('display', 'block');
-		},
+	if(fullname === ""){
+		resetErrors();
+		$('.client-name p').removeClass('hidden');
+		$('.client-name input').css('border', '2px solid red')
+	}
+	else if(!pattern.test(email)){
+		resetErrors();
+		$('.client-email p').removeClass('hidden');
+		$('.client-email input').css('border', '2px solid red')
+	}
+	else if(message === ""){
+		resetErrors();
+		$('.client-message p').removeClass('hidden');
+		$('.client-message input').css('border', '2px solid red')
+	}else{
+		$.ajax('https://alizzens-services.herokuapp.com/sendEmail',
+		{	
+			beforeSend: function(){
+				resetErrors();
+				$('#msg-status').css('display', 'block');
+				$('#submit-button').css('display', 'none');
+			},
+			type:"POST",
+			contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+			data:{
+				message: message,
+				email: email,
+				fullname: fullname
+			},
 
-		error: function( xhr, status, errText ){
-			$('form').css('display', 'none')
-			$('.fail').css('display', 'block');
-		}
+			success: function(a,b,c){
+				$('form').css('display', 'none')
+				$('#msg-status').css('display', 'none')
+				$('.success').css('display', 'block');
+			},
 
-	})
+			error: function( xhr, status, errText ){
+				$('form').css('display', 'none');
+				$('#msg-status').css('display', 'none');
+				$('.fail').css('display', 'block');
+			}
+
+		})
+	}
+
+	
+
 }
 
 function main(){
